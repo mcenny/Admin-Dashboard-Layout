86906bfbd44bfdfc93a97eb1675893d196848e5a
Philemon Eniola <dev.mcenny@gmail.com>
2023-03-01 09:35:29 +0100

    fixed the type error

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -4,58 +4,64 @@ import Link from 'next/link'
 import { HomeIcon, CreditCardIcon, UserIcon } from '@heroicons/react/24/solid';
 import { useRouter } from 'next/router';
 
-const SideBar = forwardRef<HTMLDivElement>(({ showNav }: any, ref) => {
-    const router = useRouter();
-    return (
-        <div ref={ref} className='fixed w-56 h-full bg-white shadow-sm'>
-            <div className="flex justify-center mt-6 mb-14">
-                <picture>
-                    <source srcSet='/next.svg' type='image/webp' />
-                    <img src="/next.svg" alt="Next Logo" className='w-32 h-auto' />
-                </picture>
-            </div>
+type SideBarProps = {
+    showNav: boolean;
+}
 
-            <div className="flex flex-col">
-                <Link href={'/'}>
-                    <div
-                        className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/' ? 'bg-orange-100 text-orange-500' : 'text-gray-400 hover:bg-orange-100 hover:text-orange-500'}`}
-                    >
-                        <div className="mr-2">
-                            <HomeIcon className='h-5 w-5' />
+const SideBar = forwardRef<HTMLDivElement, SideBarProps>(
+    ({ showNav }: any, ref) => {
+        const router = useRouter();
+        if (showNav) { }
+        return (
+            <div ref={ref} className='fixed w-56 h-full bg-white shadow-sm'>
+                <div className="flex justify-center mt-6 mb-14">
+                    <picture>
+                        <source srcSet='/next.svg' type='image/webp' />
+                        <img src="/next.svg" alt="Next Logo" className='w-32 h-auto' />
+                    </picture>
+                </div>
+
+                <div className="flex flex-col">
+                    <Link href={'/'}>
+                        <div
+                            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/' ? 'bg-orange-100 text-orange-500' : 'text-gray-400 hover:bg-orange-100 hover:text-orange-500'}`}
+                        >
+                            <div className="mr-2">
+                                <HomeIcon className='h-5 w-5' />
+                            </div>
+                            <div>
+                                <p>Home</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>Home</p>
+                    </Link>
+                    <Link href={'/account'}>
+                        <div
+                            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/account' ? 'bg-orange-100 text-orange-500' : 'text-gray-400 hover:bg-orange-100 hover:text-orange-500'}`}
+                        >
+                            <div className="mr-2">
+                                <UserIcon className='h-5 w-5' />
+                            </div>
+                            <div>
+                                <p>Account</p>
+                            </div>
                         </div>
-                    </div>
-                </Link>
-                <Link href={'/account'}>
-                    <div
-                        className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/account' ? 'bg-orange-100 text-orange-500' : 'text-gray-400 hover:bg-orange-100 hover:text-orange-500'}`}
-                    >
-                        <div className="mr-2">
-                            <UserIcon className='h-5 w-5' />
+                    </Link>
+                    <Link href={'/billing'}>
+                        <div
+                            className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/billing' ? 'bg-orange-100 text-orange-500' : 'text-gray-400 hover:bg-orange-100 hover:text-orange-500'}`}
+                        >
+                            <div className="mr-2">
+                                <CreditCardIcon className='h-5 w-5' />
+                            </div>
+                            <div>
+                                <p>Billing</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>Account</p>
-                        </div>
-                    </div>
-                </Link>
-                <Link href={'/billing'}>
-                    <div
-                        className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${router.pathname == '/billing' ? 'bg-orange-100 text-orange-500' : 'text-gray-400 hover:bg-orange-100 hover:text-orange-500'}`}
-                    >
-                        <div className="mr-2">
-                            <CreditCardIcon className='h-5 w-5' />
-                        </div>
-                        <div>
-                            <p>Billing</p>
-                        </div>
-                    </div>
-                </Link>
+                    </Link>
+                </div>
             </div>
-        </div>
-    )
-})
+        )
+    })
 
 SideBar.displayName = 'SideBar';
 
